3122a44ed266f27d93241686804eee67a9ed6509
Brian Vegetabile <bvegetabile@gmail.com>
2018-02-01 11:44:15 -0800

    Finished the first challenge: Found packages and wrote them to installed-packages.csv

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -2,6 +2,8 @@
 
 ## create a data frame of your installed packages
 
+pkgs <- installed.packages()
+
 ## keep the variables
 ##   * Package
 ##   * LibPath
@@ -9,10 +11,21 @@
 ##   * Priority
 ##   * Built
 
+pkgs <- pkgs[,colnames(pkgs) %in% c("Package",
+                                    "LibPath",
+                                    "Version",
+                                    "Priority",
+                                    "Built")]
+
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+write.csv(pkgs,
+          here::here('data', "installed-packages.csv"),
+          row.names = F,
+          quote = F)
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
